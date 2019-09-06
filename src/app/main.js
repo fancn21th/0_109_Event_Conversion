328d19780fb65dc9c4bb20d96f366867b160cf37
alex.fan <alex.fan@okchem.com>
2019-09-06 19:01:43 +0800

    feature: pass touch event

--- src/app/main.js
+++ src/app/main.js
@@ -1,7 +1,27 @@
 var top = document.getElementById("top");
 var bottom = document.getElementById("bottom");
 
-bottom.addEventListener(
+var simulateTouchEvent = function(eventType, e, element) {
+  try {
+    let touchEvent = new TouchEvent(eventType, {
+      touches: e.touches,
+      targetTouches: e.targetTouches,
+      changedTouches: e.changedTouches
+    });
+    touchEvent.fromBorder = true;
+    element.dispatchEvent(touchEvent);
+  } catch (err) {
+    let touchEvent = document.createEvent("Event");
+    touchEvent.initEvent(eventType, true, true);
+    touchEvent.touches = e.touches;
+    touchEvent.targetTouches = e.targetTouches;
+    touchEvent.changedTouches = e.changedTouches;
+    touchEvent.fromBorder = true;
+    element.dispatchEvent(touchEvent);
+  }
+};
+
+top.addEventListener(
   "mousedown",
   function(e) {
     e.stopPropagation();
@@ -11,10 +31,29 @@ bottom.addEventListener(
 );
 
 top.addEventListener(
+  "touchstart",
+  function(e) {
+    e.stopPropagation();
+    console.log("touch start", e.target);
+    simulateTouchEvent("touchstart", e, bottom);
+  },
+  true
+);
+
+bottom.addEventListener(
   "mousedown",
   function(e) {
     e.stopPropagation();
     console.log(e.target);
+  },
+  true
+);
+
+bottom.addEventListener(
+  "touchstart",
+  function(e) {
+    e.stopPropagation();
+    console.log("touch start", e.target);
   },
   true
 );
