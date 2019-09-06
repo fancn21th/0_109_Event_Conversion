1f6c86677a870f0a5cb2d5558fed2f891dea3c20
alex.fan <alex.fan@okchem.com>
2019-09-06 18:19:49 +0800

    feature: basic event

--- src/app/main.js
+++ src/app/main.js
@@ -1,1 +1,20 @@
-console.log("hello world!");
+var top = document.getElementById("top");
+var bottom = document.getElementById("bottom");
+
+bottom.addEventListener(
+  "mousedown",
+  function(e) {
+    e.stopPropagation();
+    console.log(e.target);
+  },
+  true
+);
+
+top.addEventListener(
+  "mousedown",
+  function(e) {
+    e.stopPropagation();
+    console.log(e.target);
+  },
+  true
+);
